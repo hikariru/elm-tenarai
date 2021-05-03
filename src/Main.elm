c844cc6c60a947aa6e494a0b12142db66be15db3
Hikaru Uchiyama <hkr.uchym@gmail.com>
2021-05-03 22:10:29 +0900

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,22 @@
+module Main exposing (main)
+
+import Html exposing (text)
+
+main =
+        text (bottlesOfJuice 99)
+
+greeting: String
+greeting =
+    "static elm"
+
+sayHello: String -> String
+sayHello name =
+    "Hello " ++ name ++ "."
+
+bottlesOf: String -> (Int -> String)
+bottlesOf contents amount =
+    Debug.toString amount ++ " bottles of " ++ contents ++ " on the wall."
+
+bottlesOfJuice: Int -> String
+bottlesOfJuice  =
+    bottlesOf "Juice"
