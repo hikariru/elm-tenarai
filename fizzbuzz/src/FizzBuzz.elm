943fe3f5574b7de0e8cf9e58ad2f2780e5b1599d
Hikaru Uchiyama <hkr.uchym@gmail.com>
2021-05-23 09:24:28 +0900

    とりさん先生にFizzBuzzできるね？って言われた。つらかった。

---
+++ fizzbuzz/src/FizzBuzz.elm
@@ -0,0 +1,29 @@
+module FizzBuzz exposing (..)
+
+import Html exposing (..)
+
+main : Html msg
+main =
+    div []
+    [ ul []
+        (List.range 1 15
+            |> List.map fizzbuzz)
+    ]
+
+fizzbuzz : Int -> Html msg
+fizzbuzz num =
+    let
+        mod =
+            (modBy 3 num, modBy 5 num)
+        str = case mod of
+            (0, 0) ->
+                "fizzbuzz"
+            (0, _) ->
+               "fizz"
+            (_, 0) ->
+                "buzz"
+            _ ->
+               String.fromInt num
+    in
+        li []
+            [ text str ]
